133287b6221cbce7029b33911e7fa1a750393782
Michael Dinh <mkdinh94@gmail.com>
2021-06-23 19:54:03 -0400

    add mongodb connection

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, jsonify, render_template
 from dotenv import load_dotenv
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+app.config['MONGO_URI'] = environ.get('MONGODB_URI')
 
 # database setup
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,6 +31,20 @@ def note_postgres():
         data.append({
             "id": note.id,
             "content": note.content
+        })
+
+    return jsonify(data)
+
+
+@app.route('/api/notes/mongo')
+def note_mongo():
+    notes = mongo.db.notes.find()
+    data = []
+
+    for note in notes:
+        data.append({
+            '_id': str(note['_id']),
+            'content': note['content']
         })
 
     return jsonify(data)
